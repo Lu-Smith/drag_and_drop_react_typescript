b5153b4c6a81d9b2ced7445415162b2f0ad68634
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-12-05 11:13:48 +0000

    Created data.ts with listItems

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Main.css'
+import { listitems } from '../sources/data'
+
 
 const Main = () => {
+  const [todo, setTodo] = useState(listitems)
   return (
-    <div className='Main'>Main</div>
+    <div className='Main'>
+      Main
+    </div>
   )
 }
 
---
+++ src/sources/data.ts
@@ -0,0 +1,30 @@
+export const listitems = [
+    {
+      id: '1',
+      name: 'breakfast'
+    },
+    {
+      id: '2',
+      name: 'coding'
+    },
+    {
+      id: '3',
+      name: 'lunch'
+    },
+    {
+      id: '4',
+      name: 'more coding'
+    },
+    {
+      id: '5',
+      name: 'dinner'
+    },
+    {
+      id: '6',
+      name: 'C# course'
+    },
+    {
+      id: '7',
+      name: 'walk the dog'
+    },
+  ]
